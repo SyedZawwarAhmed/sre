40c1709a7d98e178903fb2359b318cd8c3fb8749
Forhad Hosain <forhad@smythos.com>
2025-08-26 03:41:34 +0600

    remove unnecessary const "O3_AND_O4_MODELS_PATTERN"

--- packages/core/src/subsystems/LLMManager/LLM.service/connectors/openai/apiInterfaces/constants.ts
+++ packages/core/src/subsystems/LLMManager/LLM.service/connectors/openai/apiInterfaces/constants.ts
@@ -1,5 +1,4 @@
 export const O3_AND_O4_MODELS = ['o3', 'o3-pro', 'o4-mini'];
-export const O3_AND_O4_MODELS_PATTERN = /o3|o4/i;
 export const MODELS_WITHOUT_JSON_RESPONSE_SUPPORT = ['o1-preview'];
 export const MODELS_WITHOUT_SYSTEM_MESSAGE_SUPPORT = ['o1-mini', 'o1-preview'];
 
